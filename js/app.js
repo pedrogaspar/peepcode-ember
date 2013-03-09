ff0b2980931b3c093fe49fa7ed53bfcf9cf2b09c
Pedro <pedro.gaspar@wiremaze.com>
2013-03-09 12:36:54 +0000

    Add comments to explain ember parts

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 var App = Ember.Application.create();
 
 // Router
+// Smilar to rails routes
 App.Router.map(function() {
   this.resource('tables'); // '/#/tables'
 });
 
+// Routes
+// Run code when urls are visited and load data and assign to a controller
 App.TablesRoute = Ember.Route.extend({ // Extend like inheritance
   model: function() {
     return App.Table.find(); // Without parameters to return all tables
@@ -12,9 +15,11 @@ App.TablesRoute = Ember.Route.extend({ // Extend like inheritance
 });
 
 // Controllers
+// Deliver model data to views and templates. Have a model property
 App.TablesController = Ember.ArrayController.extend(); // Must match the route name
 
 // Models
+// Store specify how we access data
 App.Store = DS.Store.extend({
   revision: 11, // Ember.Data has versions, this specifies the latest
   adapter: 'DS.FixtureAdapter' // Loads data locally. Good to initial setup
